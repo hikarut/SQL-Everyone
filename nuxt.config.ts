c233a6b84a0af6d3975b2f9d14f787f3fb57b98e
hikarut <ht.hikaru.takahashi@gmail.com>
2024-11-16 17:38:06 +0900

    [update] meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,9 +14,54 @@ export default defineNuxtConfig({
     head: {
       // ページのタイトルやメタタグの設定
       title: 'データ分析内製化に向けた人材育成サービス『SQL Everyone』',
+      // メタタグ
       meta: [
-        // メタタグの例
-        { name: 'description', content: 'データ分析内製化に向けた人材育成サービス『SQL Everyone』' },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content:
+            'SQL,SQL研修サービス,ChatGPT,生成AI,データ分析内製化,研修,法人向け,勉強会,データ分析,データサイエンティスト,データアナリスト,データエンジニア,マーケター,営業,ビジネスパーソン',
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            '『SQL Everyone』はデータ分析内製化に向けた人材育成サービスです。目的・レベルに合わせたSQL分析人材を育成することでデータ分析内製化を支援します。SQLを全く知らない人が受けてもデータ分析で使えるSQLが学べるように、『集合研修プラン』と『マンツーマントレーニングプラン』の2つのサービスを提供しています。',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: `https://sql-everyone.vercel.app/ogimage.jpeg`,
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'データ分析内製化に向けた人材育成サービス『SQL Everyone』',
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://sql-everyone.vercel.app/',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            '『SQL Everyone』はデータ分析内製化に向けた人材育成サービスです。目的・レベルに合わせたSQL分析人材を育成することでデータ分析内製化を支援します。SQLを全く知らない人が受けてもデータ分析で使えるSQLが学べるように、『集合研修プラン』と『マンツーマントレーニングプラン』の2つのサービスを提供しています。',
+        },
+        {
+          hid: 'twitter:card',
+          property: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          name: 'application-name',
+          content: 'データ分析内製化に向けた人材育成サービス『SQL Everyone』',
+        },
+        {
+          name: 'apple-mobile-web-app-title',
+          content: 'データ分析内製化に向けた人材育成サービス『SQL Everyone』',
+        },
       ],
       link: [
         // ファビコンの設定
