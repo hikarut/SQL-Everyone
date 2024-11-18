6f6df249aacc09cc7ed6f2ebcecf37d844516b9d
hikarut <ht.hikaru.takahashi@gmail.com>
2024-11-18 19:51:39 +0900

    [update] og image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
         {
           hid: 'og:image',
           property: 'og:image',
-          content: `https://sql-everyone.vercel.app/ogimage.jpeg`,
+          content: `https://sql-everyone.vercel.app/ogimage.png`,
         },
         {
           hid: 'og:title',
