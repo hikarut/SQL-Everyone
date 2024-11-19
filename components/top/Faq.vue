8902d80dac21103176f996a4914c25dc98f04152
hikarut <ht.hikaru.takahashi@gmail.com>
2024-11-19 20:53:58 +0900

    [update] FAQ

--- components/top/Faq.vue
+++ components/top/Faq.vue
@@ -186,6 +186,57 @@
             </div>
           </div>
 
+          <div
+            class="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 dark:hs-accordion-active:bg-white/10"
+            id="hs-basic-with-title-and-arrow-stretched-heading-four"
+          >
+            <button
+              class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-start text-gray-800 rounded-lg transition hover:text-gray-500 focus:outline-none focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400"
+              aria-expanded="false"
+              aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-four"
+            >
+              個人でSQLトレーニングを受けることはできますか？
+              <svg
+                class="hs-accordion-active:hidden block shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="m6 9 6 6 6-6" />
+              </svg>
+              <svg
+                class="hs-accordion-active:block hidden shrink-0 size-5 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <path d="m18 15-6-6-6 6" />
+              </svg>
+            </button>
+            <div
+              id="hs-basic-with-title-and-arrow-stretched-collapse-four"
+              class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
+              role="region"
+              aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-four"
+            >
+              <p class="text-gray-600 dark:text-neutral-200">
+                基本的には法人（会社）向けのサービスですので、まずは所属する会社にご相談いただき法人としてお問い合わせください。それでも個人でサービスを受けたい場合は支援内容など個別に調整できる場合もございますので、一度お問い合わせフォームからご連絡ください。
+              </p>
+            </div>
+          </div>
+
         </div>
         <!-- End Accordion -->
       </div>
