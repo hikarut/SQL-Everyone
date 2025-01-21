ec5dbeb6f8d3ca420870dcb8297cd110f61e62da
hikarut <ht.hikaru.takahashi@gmail.com>
2025-01-22 08:32:15 +0900

    [update] code

--- pages/articles/[id].vue
+++ pages/articles/[id].vue
@@ -1,24 +1,61 @@
 <template>
-  <!-- Card Blog -->
-  <div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <!-- Blog Article -->
+  <div class="max-w-3xl px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto">
+    <div class="max-w-2xl">
+      <!-- Content -->
+      <div class="space-y-5 md:space-y-8">
+        <figure>
+          <img
+            class="w-full object-cover rounded-xl"
+            :src="data.eyecatch?.url"
+            :width="data.eyecatch?.width"
+            :height="data.eyecatch?.height"
+            alt=""
+          />
+        </figure>
 
-    <img
-      :src="data.eyecatch?.url"
-      :width="data.eyecatch?.width"
-      :height="data.eyecatch?.height"
-      alt=""
-    />
+        <h1
+          class="font-bold text-xl lg:text-4xl text-gray-800 dark:text-neutral-200 text-center mb-10 mt-10"
+        >
+          {{ data.title }}
+        </h1>
 
-    <h1
-      class="font-bold text-xl lg:text-4xl text-gray-800 dark:text-neutral-200 text-center mb-10 mt-10"
-    >
-      {{ data.title }}
-    </h1>
+        <div
+          v-html="data.content"
+          class="text-lg text-gray-800 dark:text-neutral-200 space-y-6 prose"
+        ></div>
 
-    <div v-html="data.content"></div>
-
+        <div>
+          <a
+            class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+            href="#"
+          >
+            Plan
+          </a>
+          <a
+            class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+            href="#"
+          >
+            Web development
+          </a>
+          <a
+            class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+            href="#"
+          >
+            Free
+          </a>
+          <a
+            class="m-1 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+            href="#"
+          >
+            Team
+          </a>
+        </div>
+      </div>
+      <!-- End Content -->
+    </div>
   </div>
-  <!-- End Card Blog -->
+  <!-- End Blog Article -->
 </template>
 
 <script setup lang="ts">
